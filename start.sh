6f35b49c43f48ea576cab088be448cce578af789
hejz <75412985@qq.com>
2023-01-11 22:39:08 +0800

    修复start.sh

--- start.sh
+++ start.sh
@@ -5,14 +5,15 @@ cd ~
 rm -rf springboot-netty
 #git拉取代码
 git clone git@github.com:wskvfhprrk/springboot-netty.git
+cd /root/springboot-netty/
+cp -rf start.sh /root/
 #进入目录中
-cd springboot-netty/studay/
+cd studay
 #跳过测试建jar包
 mvn clean package -Dmaven.test.skip=true
 
 #重启服务
 cd /root/springboot-netty/studay/target
 nohup java -jar -Dserver.port=8080 studay-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
-cd /root/springboot-netty/
-cp -rf start.sh /root/
+
 #nohup java -jar -Dserver.port=8080 /root/springboot-netty/studay/target/studay-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
