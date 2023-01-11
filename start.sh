962b8cb30a4c189017b6f8ceff3428de9ab49448
hejz <75412985@qq.com>
2023-01-11 16:51:13 +0800

    修复maven构建时错误

---
+++ start.sh
@@ -0,0 +1,9 @@
+cd ~
+#删除文件
+rm -rf /springboot-netty
+#git拉取代码
+git clone git@github.com:wskvfhprrk/springboot-netty.git
+#进入目录中
+cd springboot-netty/studay/
+#跳过测试建jar包
+mvn clean package -Dmaven.test.skip=true
