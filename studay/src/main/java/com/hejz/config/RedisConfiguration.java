571ed2210deb8804970344d534aa3e85a39e65dd
hejz <75412985@qq.com>
2023-01-14 17:37:32 +0800

    @Cacheable设置过期时间

--- studay/src/main/java/com/hejz/config/RedisConfiguration.java
+++ studay/src/main/java/com/hejz/config/RedisConfiguration.java
@@ -11,10 +11,8 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCache;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -22,11 +20,8 @@ import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.util.StringUtils;
 
 import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author 何建哲
@@ -110,5 +105,4 @@ public class RedisConfiguration extends CachingConfigurerSupport {
         template.afterPropertiesSet();
         return template;
     }
-
 }
