280765ac94c84f6585ae587cd48a41bb431f0f27
hejz <75412985@qq.com>
2023-01-25 12:39:54 +0800

    重置代码——解决粘包和拆包问题

--- studay/src/main/java/com/hejz/nettyserver/NettyMsgDecoder.java
+++ studay/src/main/java/com/hejz/nettyserver/NettyMsgDecoder.java
@@ -39,49 +39,56 @@ public class NettyMsgDecoder extends MessageToMessageDecoder<byte[]> {
         AttributeKey<Long> key = AttributeKey.valueOf(Constant.CHANNEl_KEY);
         Long dtuId = ctx.channel().attr(key).get();
         DtuInfo dtuInfo = dtuInfoService.findById(dtuId);
-        //指令的地址值
-        Integer adds = 0;
-        if (dtuInfo.getNoImei()) {
-            //imei在前取出imei长度+1位就是地址位令仅为一位
-            Integer useLength = 0;
-            byte[] b = new byte[1];
-            System.arraycopy(bytes, Constant.IMEI_LENGTH, b, 0, 1);  //数组截取
-            String hex = "0x" + HexConvert.BinaryToHexString(b).replaceAll(" ", "");
-            adds = Integer.parseInt(hex.substring(2), 16);//从第2个字符开始截取
-        } else {
-            Integer useLength = 0;
-            byte[] b = new byte[1];
-            System.arraycopy(bytes, 0, b, 0, 1);  //数组截取
-            String hex = "0x" + HexConvert.BinaryToHexString(b).replaceAll(" ", "");
-            adds = Integer.parseInt(hex.substring(2), 16);//从第2个字符开始截取
-        }
-        //有用数据建立在dtu中地址
-        Integer useLength = 0;
-        String relayCheckingRulesIds = dtuInfo.getRelayCheckingRulesIds() + "," + dtuInfo.getSensorCheckingRulesIds();
-        for (String s : relayCheckingRulesIds.split(",")) {
-            String[] split = s.split("-");
-            if (Integer.valueOf(split[0]).equals(adds)) {
-                CheckingRules checkingRules = checkingRulesService.findById(Integer.valueOf(split[1]));
-                useLength = checkingRules.getCommonLength();
-                break;
+        String[] s1 = HexConvert.BinaryToHexString(bytes).replaceAll(" ", "").split(HexConvert.convertStringToHex(dtuInfo.getImei()));
+//        if(s1.length==2){
+//            Thread.sleep(10000l);
+//        }
+//        log.info("s1.length=={}",s1.length);
+        for (String s : s1) {
+            if(s.length()!=0) {
+                //指令的地址值
+                Integer adds = 0;
+                if (dtuInfo.getNoImei()) {
+                    String hex = "0x" + s.substring(0,2);
+                    adds = Integer.parseInt(hex.substring(2), 16);//从第2个字符开始截取
+                } else {
+                    String hex = "0x" + s.substring(0,2);
+                    adds = Integer.parseInt(hex.substring(2), 16);//从第2个字符开始截取
+                }
+                //有用数据建立在dtu中地址
+                Integer useLength = 0;
+                String relayCheckingRulesIds = dtuInfo.getRelayCheckingRulesIds() + "," + dtuInfo.getSensorCheckingRulesIds();
+                for (String relayCheckingRulesId : relayCheckingRulesIds.split(",")) {
+                    String[] split = relayCheckingRulesId.split("-");
+                    if (Integer.valueOf(split[0]).equals(adds)) {
+                        CheckingRules checkingRules = checkingRulesService.findById(Integer.valueOf(split[1]));
+                        useLength = checkingRules.getCommonLength();
+                        break;
+                    }
+                }
+                if(useLength.equals(dtuInfo.getHeartbeatLength()) && adds.equals("0")){
+                    //心跳不作处理
+                    log.info("检测到客户端imei:{}心跳：{}",dtuInfo.getImei(),s);
+                }
+                //如果带有imei且大于等于总长度的话的话就可以拆包，去除imei值了
+                if (bytes.length - Constant.IMEI_LENGTH - useLength >= 0 && dtuInfo.getNoImei()) {
+                    //只要有用的才可以用
+                    byte[] useBytes = new byte[useLength];
+                    System.arraycopy(bytes, Constant.IMEI_LENGTH, useBytes, 0, useLength);  //数组截取
+                    list.add(useBytes);
+                }
+                //如果不带imei且大于等于总长度的话的话就可以拆包
+                else if (bytes.length - useLength >= 0 && !dtuInfo.getNoImei()) {
+                    //只要有用的才可以用
+                    list.add(bytes);
+                } else {
+                    //不够就等待
+                    log.info("长度不够，继续等待");
+                    return;
+                }
             }
         }
-        //如果带有imei且大于等于总长度的话的话就可以拆包，去除imei值了
-        if (bytes.length - Constant.IMEI_LENGTH - useLength >= 0 && dtuInfo.getNoImei()) {
-            //只要有用的才可以用
-            byte[] useBytes = new byte[useLength];
-            System.arraycopy(bytes, Constant.IMEI_LENGTH, useBytes, 0, useLength);  //数组截取
-            list.add(useBytes);
-        }
-        //如果不带imei且大于等于总长度的话的话就可以拆包
-        else if (bytes.length - useLength >= 0 && !dtuInfo.getNoImei()) {
-            //只要有用的才可以用
-            list.add(bytes);
-        } else {
-            //不够就等待
-            log.info("长度不够，继续等待");
-            return;
-        }
+
     }
 
 
