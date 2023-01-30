b3b1b941cd75558b705176b13842d440bf053f40
hejz <75412985@qq.com>
2023-01-31 01:22:44 +0800

    查询数据

--- dtu/src/main/java/com/hejz/service/impl/SensorDataDbServiceImpl.java
+++ dtu/src/main/java/com/hejz/service/impl/SensorDataDbServiceImpl.java
@@ -8,12 +8,11 @@ import com.hejz.repository.SensorDataDbRepository;
 import com.hejz.service.SensorDataDbService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.criteria.Predicate;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -24,57 +23,43 @@ import java.util.List;
 @Service
 public class SensorDataDbServiceImpl implements SensorDataDbService {
     @Autowired
-    private SensorDataDbRepository selayRepository;
+    private SensorDataDbRepository sensorDataDbRepository;
 
     @Override
     public List<SensorDataDb> findAllByDtuId(Long dtuId) {
-        return selayRepository.findAllByDtuId(dtuId);
+        return sensorDataDbRepository.findAllByDtuId(dtuId);
     }
 
     @Override
     public SensorDataDb getById(Long id) {
-        SensorDataDb selay = selayRepository.findById(id);
+        SensorDataDb selay = sensorDataDbRepository.findById(id);
         return selay;
     }
 
     @Override
     public SensorDataDb save(SensorDataDb selay) {
-        return selayRepository.save(selay);
+        return sensorDataDbRepository.save(selay);
     }
 
     @Override
     public SensorDataDb update(SensorDataDb selay) {
-        return selayRepository.save(selay);
+        return sensorDataDbRepository.save(selay);
     }
 
     @Override
     public void delete(Long id) {
-        selayRepository.deleteById(id);
+        sensorDataDbRepository.deleteById(id);
     }
 
     @Override
     @Transactional
     public void deleteAllByDtuId(Long dutId) {
-        selayRepository.deleteAllByDtuId(dutId);
+        sensorDataDbRepository.deleteAllByDtuId(dutId);
     }
 
     @Override
     public Result<PageResult> getPage(Page page) {
-        Specification<SensorDataDb> sp = (root, query, cb) -> {
-            List<Predicate> predicates = new ArrayList<>();
-//            if(user.getId()!=null && user.getId()!=0) {
-//                predicates.add(cb.equal(root.get("Id"), user.getId()));
-//            }
-//            if(StringUtils.isNotBlank(user.getUsername())) {
-//                predicates.add(cb.like(root.get("Username"), "%"+user.getUsername()+"%"));
-//            }
-//            if(user.getAge()!=null && user.getAge()!=0) {
-//                predicates.add(cb.equal(root.get("Age"), user.getAge()));
-//            }
-            Predicate[] andPredicate = new Predicate[predicates.size()];
-            return cb.and(predicates.toArray(andPredicate));
-        };
-        org.springframework.data.domain.Page<SensorDataDb> all = selayRepository.findAll(sp, PageRequest.of(page.getPage(), page.getLimit(), page.getSort()));
+        List<SensorDataDb> all = sensorDataDbRepository.findAll((Specification<SensorDataDb>) PageRequest.of(page.getPage(), page.getLimit(), page.getSort()));
         return Result.ok(all);
     }
 
