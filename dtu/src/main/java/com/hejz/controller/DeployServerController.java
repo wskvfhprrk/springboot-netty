28d8573f1e71fcd8a272133f037689ae2eb3e8f0
hejz <75412985@qq.com>
2023-01-31 20:09:26 +0800

    远程部署代码

--- dtu/src/main/java/com/hejz/controller/DeployServerController.java
+++ dtu/src/main/java/com/hejz/controller/DeployServerController.java
@@ -22,16 +22,10 @@ public class DeployServerController {
     @GetMapping
     @ApiOperation("部署服务器")
     public void deployServer(){
-        //p = Runtime.getRuntime().exec(SHELL_FILE_DIR + RUNNING_SHELL_FILE + " "+param1+" "+param2+" "+param3);
-        //p.waitFor();
         try {
-//            Runtime.getRuntime().exec("sh /root/start.sh").waitFor();
-//            log.info("代码重新部署服务器成功……");
-//            return "代码重新部署服务器成功！";
             Runtime rt = Runtime.getRuntime();
-//            String[] cmd = {"/bin/sh","-c","reboot"};
             String[] cmd = {"/bin/sh","-c","shutdown -r now"};
-            Process proc = rt.exec(cmd);
+            rt.exec(cmd);
 
         } catch (IOException  e) {
             log.error("代码重新部署服务器失败",e.fillInStackTrace());
