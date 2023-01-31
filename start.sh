bd19f0763d6e0e87c556bb79cef0760d1e9d68f5
hejz <75412985@qq.com>
2023-01-31 09:53:09 +0800

    远程部署代码

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@
 #echo -e "\033[3${i};4${j}m文字色值 ${i}, 背景色值 ${j}\033[0m"
 #cho -e "\033[3${0};4${7}m文字色值 ${0}, 背景色值 ${7}\033[0m"
 #后台启动小米球
-#cd /root/linux_xiaomiqu
-#nohup ./xiaomiqiu -authtoken=bAe854993e6444e3925b24c7edcdd72A -log=xiaomiqiu.log -log-level=info start-all & > /dev/null 2>&1 &
+cd /root/linux_xiaomiqu
+nohup ./xiaomiqiu -authtoken=bAe854993e6444e3925b24c7edcdd72A -log=xiaomiqiu.log -log-level=info start-all & > /dev/null 2>&1 &
 #删除服务器
 echo -e  ======================1.删除运行的服务器======================
 ps -aux | grep dtu | grep -v grep| awk '{print $2}' |xargs  kill -9
@@ -33,26 +33,26 @@ find -name 'application.yml' | xargs perl -pi -e 's|create-drop|update|g'
 find -name 'InitController.java' | xargs perl -pi -e 's|PostConstruct|GetMapping|g'
 find -name 'InitController.java' | xargs perl -pi -e 's|javax.annotation.GetMapping|javax.annotation.PostConstruct|g'
 #跳过测试建jar包
-echo -e ======================6.跳过测试构建jar包======================
+echo -e ======================7.跳过测试构建jar包======================
 mvn clean package -Dmaven.test.skip=true
 #重启服务
-echo -e ======================7.重启服务======================
+echo -e ======================8.重启服务======================
 cd target
 nohup java -jar -Dserver.port=8080 dtu-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
-echo -e
-echo -e
-echo -e =================请自行打开小米球操作======================
-echo -e
-echo -e --- "开新窗口:输入screen -S xiaomiqiu"
-echo -e
-echo -e --- "会弹出新的窗口,在里面输入启动指令"
-echo -e
-echo -e --- "启动指令:    ./xiaomiqiu -authtoken=bAe854993e6444e3925b24c7edcdd72A -log=xiaomiqiu.log -log-level=info start-all"
-echo -e
-echo -e
-echo -e
-sleep 10
-echo -e ======================8.自动打开日志======================
+#echo -e
+#echo -e
+#echo -e =================请自行打开小米球操作======================
+#echo -e
+#echo -e --- "开新窗口:输入screen -S xiaomiqiu"
+#echo -e
+#echo -e --- "会弹出新的窗口,在里面输入启动指令"
+#echo -e
+#echo -e --- "启动指令:    ./xiaomiqiu -authtoken=bAe854993e6444e3925b24c7edcdd72A -log=xiaomiqiu.log -log-level=info start-all"
+#echo -e
+#echo -e
+#echo -e
+#sleep 10
+echo -e ======================10.自动打开日志======================
 tail -f /home/logs/dtu/logback_info.log
 #接下来在服务器上安装screen
 #yum install screen
