ab0d1664306974b49b135875507f29d0e45aa9fe
hejz <75412985@qq.com>
2023-02-15 20:57:27 +0800

    代码重构

--- dtu/src/main/java/com/hejz/dtu/common/Constant.java
+++ dtu/src/main/java/com/hejz/dtu/common/Constant.java
@@ -18,14 +18,10 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class Constant {
     //数据检查规则redis缓存名称
-    public static final String CHECKING_RULES_CACHE_KEY = "checkingRulesCacheKey";
-    public static final String RELAY_CACHE_KEY = "relayCacheKey";
     public static final String SENSOR_CACHE_KEY = "sensorCacheKey";
-    public static final String RELAY_DEFINITION_COMMAND_CACHE_KEY = "relayDefinitionCommandCacheKey";
     public static final String DTU_INFO_CACHE_KEY = "dtuInfoCacheKey";
     public static final String DTU_INFO_IMEI_CACHE_KEY = "dtuInfoImeiCacheKey";
     public static final String CACHE_INSTRUCTIONS_THAT_NEED_TO_CONTINUE_PROCESSING_CACHE_KEY = "cacheInstructionsThatNeedToContinueProcessingCacheKey";
-    public static final String COMMAND_STATUS_CACHE_KEY = "commandStatusCacheKey";
     public static final String INSTRUCTION_DEFINITION_CACHE_KEY = "instructionDefinitionCacheKey";
     //最后时间——key为ctx.channel().id()
     public static final Map<String, LocalDateTime> END_TIME_MAP = new HashMap<>();
--- dtu/src/main/java/com/hejz/dtu/controller/InitController.java
+++ dtu/src/main/java/com/hejz/dtu/controller/InitController.java
@@ -47,17 +47,9 @@ public class InitController {
     public void initData() {
         start();
         //清除所有缓存
-        Set<String> keys = redisTemplate.keys(Constant.CHECKING_RULES_CACHE_KEY + ":*");
-        redisTemplate.delete(keys);
-        keys = redisTemplate.keys(Constant.RELAY_CACHE_KEY + "*");
-        redisTemplate.delete(keys);
-        keys = redisTemplate.keys(Constant.SENSOR_CACHE_KEY + ":*");
-        redisTemplate.delete(keys);
-        keys = redisTemplate.keys(Constant.RELAY_DEFINITION_COMMAND_CACHE_KEY + "*");
+        Set<String> keys = redisTemplate.keys(Constant.SENSOR_CACHE_KEY + ":*");
         redisTemplate.delete(keys);
         keys = redisTemplate.keys(Constant.DTU_INFO_CACHE_KEY + ":*");
-        redisTemplate.delete(keys);
-        keys = redisTemplate.keys(Constant.COMMAND_STATUS_CACHE_KEY + ":*");
         redisTemplate.delete(keys);
         keys = redisTemplate.keys(Constant.CACHE_INSTRUCTIONS_THAT_NEED_TO_CONTINUE_PROCESSING_CACHE_KEY + ":*");
         redisTemplate.delete(keys);
@@ -99,12 +91,6 @@ public class InitController {
         for (int i = 0; i < 30; i++) {
             DtuInfo dtuInfo = dtuInfoRepository.save(new DtuInfo(imei.toString(), 89, 30000, true, "1,1,2,2,2,2,2,2,2"));
 
-            //处理继电器信息
-//        relayRepository.save(new Relay((long) (i + 1), 3, "第1个继电器", "03 05 00 00 FF 00 8D D8", "03 05 00 00 00 00 CC 28", "lcaolhost:8080/hello", "大棚电机双锁开关"));
-//        relayRepository.save(new Relay((long) (i + 1), 3, "第2个继电器", "03 05 00 01 FF 00 DC 18", "03 05 00 01 00 00 9D E8", "lcaolhost:8080/hello", "大棚电机总开关"));
-//        relayRepository.save(new Relay((long) (i + 1), 3, "第3个继电器", "03 05 00 02 FF 00 2C 18", "03 05 00 02 00 00 6D E8", "lcaolhost:8080/hello", "浇水电阀开关"));
-//        relayRepository.save(new Relay((long) (i + 1), 3, "第4个继电器", "03 05 00 03 FF 00 7D D8", "03 05 00 03 00 00 3C 28", "lcaolhost:8080/hello", "备用"));
-//        List<Relay> relays = relayRepository.findAlByDtuInfo((long) (i + 1)).stream().sorted(Comparator.comparing(Relay::getId)).collect(Collectors.toList());
             //处理编辑继电器命令的信息
             Set<Command> set = new HashSet<>();
             set.add(command4);
@@ -157,20 +143,6 @@ public class InitController {
             sensorRepository.save(new Sensor(dtuInfo,"土壤钾",25,15,null,null,command16,8, "mg/L"));
             sensorRepository.save(new Sensor(dtuInfo,"土壤电导率",25,15,null,null,command17,9,"us/cm"));
 
-//        List<InstructionDefinition> relayDefinitionCommands = relayDefinitionCommandRepository.findByDtuInfo((long) (i + 1));
-//        Optional<InstructionDefinition> open = relayDefinitionCommands.stream().filter(r -> r.getName().equals("打开通风指令")).findFirst();
-//        if (!open.isPresent()) return;
-//        Optional<InstructionDefinition> close = relayDefinitionCommands.stream().filter(r -> r.getName().equals("关闭通风指令")).findFirst();
-//        sensorRepository.save(new Sensor((long) (i + 1), 1, "空气温度 ", "01 03 03 00 00 01 84 4E", "D/10", "ºC", 25, 15, open.get().getId(), close.get().getId()));
-//        sensorRepository.save(new Sensor((long) (i + 1), 1, "空气湿度 ", "01 03 03 01 00 01 D5 8E", "D/10", "%", 90, 70, 0L, 0L));
-//        sensorRepository.save(new Sensor((long) (i + 1), 2, "土壤PH  ", "02 03 02 03 00 01 75 81", "D/10", "", 9, 6, 0L, 0L));
-//        sensorRepository.save(new Sensor((long) (i + 1), 2, "土壤温度 ", "02 03 02 00 00 01 85 81", "D/100+5", "ºC", 25, 25, 0L, 0L));
-//        sensorRepository.save(new Sensor((long) (i + 1), 2, "土壤湿度 ", "02 03 02 01 00 01 D4 41", "D/100", "%", 100, 80, 0L, 0L));
-//        sensorRepository.save(new Sensor((long) (i + 1), 2, "土壤氮   ", "02 03 02 04 00 01 C4 40", "D/1", "mg/L", 100, 50, 0L, 0L));
-//        sensorRepository.save(new Sensor((long) (i + 1), 2, "土壤磷   ", "02 03 02 05 00 01 95 80", "D/1", "mg/L", 100, 50, 0L, 0L));
-//        sensorRepository.save(new Sensor((long) (i + 1), 2, "土壤钾   ", "02 03 02 06 00 01 65 80", "D/1", "mg/L", 100, 50, 0L, 0L));
-//        sensorRepository.save(new Sensor((long) (i + 1), 2, "土壤电导率", "02 03 02 02 00 01 24 41", "D/1", "us/cm", 250, 80, 0L, 0L));
-//        //处理dtu信息
             imei++;
         }
 
