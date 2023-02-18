11c367954ed22ac53d44c2334abeb42d051c1c59
hejz <75412985@qq.com>
2023-02-18 21:55:49 +0800

    添加远程部署项目

--- dtu/src/main/java/com/hejz/dtu/controller/RemoteDeploymentServer.java
+++ dtu/src/main/java/com/hejz/dtu/controller/RemoteDeploymentServer.java
@@ -4,17 +4,9 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 /**
  * @author:hejz 75412985@qq.com
@@ -26,13 +18,20 @@ import java.net.URISyntaxException;
 @Slf4j
 public class RemoteDeploymentServer {
 
-    @Autowired
-    private RestTemplate restTemplate;
-
     @GetMapping("remote")
     @ApiOperation("远程部署服务器")
     public void run() throws Exception {
         log.info("项目部署………………………………………………");
-        restTemplate.getForEntity("http://www.localhost:8090",null);
+        String command = "shutdown -r now";
+        String[] cmdStrings = new String[]{"sh", "-c", command};
+        String retString = "";
+
+        Process p = null;
+        p = Runtime.getRuntime().exec(cmdStrings);
+        int status = p.waitFor();
+        if (status != 0) {
+            System.err.println(String.format("runShellCommand: %s, status: %s", command,
+                    status));
+        }
     }
 }
