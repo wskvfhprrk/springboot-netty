45ac77c31b922dda55184c5eb7eaebe37f2489b8
hejz <75412985@qq.com>
2023-03-03 15:01:45 +0800

    echarts

---
+++ dtu/src/test/java/com/hejz/dtu/service/DtuInfoServiceTest.java
@@ -0,0 +1,39 @@
+package com.hejz.dtu.service;
+
+import com.hejz.dtu.entity.DtuInfo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DtuInfoServiceTest {
+
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findPage() {
+    }
+
+    @Test
+    void findByImei() {
+    }
+
+    @Test
+    void findAll() {
+    }
+}
